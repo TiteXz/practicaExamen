77e2734edd283ea5797944b000a01d5aaa848d6f
iker <iker.zamorano18@gmail.com>
2023-11-17 10:30:17 +0100

    ver e insertar mejorados

--- resources/views/insertar.blade.php
+++ resources/views/insertar.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 <x-guest-layout> 
 
     <form method="POST" action="{{ route('insertar-coche') }}"> @csrf <!-- Name --> <div> <x-input-label for="marca"
--- resources/views/verCoches.blade.php
+++ resources/views/verCoches.blade.php
@@ -1,24 +1,32 @@
-<x-app-layout> <x-slot name="header"> <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-    {{ __('Dashboard') }}
-    </h2>
+<x-app-layout> <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Coches') }}
+        </h2>
     </x-slot>
 
     <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900">
-        <p>Coches:</p>
-        {{ __("MARCA NOMBRE COLOR PRECIO") }}
-        <br>
-        @foreach($coches as $coche)
-        {{$coche->marca}}
-        {{$coche->nombre}}
-        {{$coche->color}}
-        {{$coche->precio}}
-        <br>
-        @endforeach
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <table style="padding">
+                        <tr>
+                            <th>MARCA</th>
+                            <th>NOMBRE</th>
+                            <th>COLOR</th>
+                            <th>PRECIO</th>
+                        </tr>
+                        @foreach($coches as $coche)
+                        <tr>
+                            <td>{{$coche->marca}}</td>
+                            <td>{{$coche->nombre}}</td>
+                            <td>{{$coche->color}}</td>
+                            <td>{{$coche->precio}}</td>
+                        </tr>
+                        @endforeach
+                    </table>
+
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-    </div>
-    </div>
-    </x-app-layout>
+</x-app-layout>
